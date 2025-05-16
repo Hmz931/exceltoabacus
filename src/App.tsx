24cd5fc76d383ea86196295754604a51d6c653bf
Hamza Bouguerra <47541283+Hmz931@users.noreply.github.com>
2025-05-16 11:54:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Footer from "@//ui/components/Footer"; // ✅ Import du footer
+import Footer from "@/components/Footer";
 
 const queryClient = new QueryClient();
 
