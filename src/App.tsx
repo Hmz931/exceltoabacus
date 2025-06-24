d0e1adbfb0f6c69a5b45c8984fc001e39f46f74e
Hamza Bouguerra <47541283+Hmz931@users.noreply.github.com>
2025-06-24 09:45:06 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ const App = () => (
           <footer className="bg-gray-50 border-t mt-12">
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
               <p className="text-center text-sm text-gray-600">
-                © 2025 Hamza Bouguerra | Internal use only | All rights reserved.
+                © 2025 <a href="https://github.com/Hmz931">Hamza Bouguerra</a> | Internal use only | All rights reserved.                
               </p>
             </div>
           </footer>
